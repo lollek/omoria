be10b9a3c2153ad8378f8d2c885776c7e825159d
Olle Kvarnström <olle+git@iix.se>
2018-05-06 10:33:54 +0200

    move c -> rust

--- src/classes.rs
+++ src/classes.rs
@@ -12,6 +12,88 @@ pub enum Class {
     Monk = 9,
 }
 
+#[derive(PartialEq)]
+pub enum MagicType {
+    Arcane = 0,
+    Divine = 1,
+    Nature = 2,
+    Song = 3,
+    Chakra = 4,
+}
+
+static WARRIOR_STRING: &'static [u8] = b"Warrior\0";
+static MAGE_STRING: &'static [u8] = b"Mage\0";
+static PRIEST_STRING: &'static [u8] = b"Priest\0";
+static ROGUE_STRING: &'static [u8] = b"Rogue\0";
+static RANGER_STRING: &'static [u8] = b"Ranger\0";
+static PALADIN_STRING: &'static [u8] = b"Paladin\0";
+static DRUID_STRING: &'static [u8] = b"Druid\0";
+static BARD_STRING: &'static [u8] = b"Bard\0";
+static ADVENTURER_STRING: &'static [u8] = b"Adventurer\0";
+static MONK_STRING: &'static [u8] = b"Monk\0";
+
+static WARRIOR_STATS: &'static [i8] = &[5, -2, -2, 2, 2, 1];
+static MAGE_STATS: &'static [i8] = &[-5, 3, 0, 0, -2, 0];
+static PRIEST_STATS: &'static [i8] = &[0, -3, 3, -1, 1, 2];
+static ROGUE_STATS: &'static [i8] = &[2, 0, -2, 3, 1, 1];
+static RANGER_STATS: &'static [i8] = &[2, 0, 1, 1, 1, 2];
+static PALADIN_STATS: &'static [i8] = &[3, -3, 1, 0, 2, 2];
+static DRUID_STATS: &'static [i8] = &[-1, -1, 2, 0, 0, 3];
+static BARD_STATS: &'static [i8] = &[2, 1, -1, 2, 0, 3];
+static ADVENTURER_STATS: &'static [i8] = &[2, 2, -1, 1, 1, 0];
+static MONK_STATS: &'static [i8] = &[2, 0, 2, 3, 1, 1];
+
+#[no_mangle]
+pub extern fn class_title(class: i32) -> *const u8 {
+    match class {
+        x if x == Class::Warrior as i32 => WARRIOR_STRING.as_ptr(),
+        x if x == Class::Mage as i32 => MAGE_STRING.as_ptr(),
+        x if x == Class::Priest as i32 => PRIEST_STRING.as_ptr(),
+        x if x == Class::Rogue as i32 => ROGUE_STRING.as_ptr(),
+        x if x == Class::Ranger as i32 => RANGER_STRING.as_ptr(),
+        x if x == Class::Paladin as i32 => PALADIN_STRING.as_ptr(),
+        x if x == Class::Druid as i32 => DRUID_STRING.as_ptr(),
+        x if x == Class::Bard as i32 => BARD_STRING.as_ptr(),
+        x if x == Class::Adventurer as i32 => ADVENTURER_STRING.as_ptr(),
+        x if x == Class::Monk as i32 => MONK_STRING.as_ptr(),
+        _ => panic!("Unknown class received"),
+    }
+}
+
+#[no_mangle]
+pub extern fn class_stats(class: i32) -> *const i8 {
+    match class {
+        x if x == Class::Warrior as i32 => WARRIOR_STATS.as_ptr(),
+        x if x == Class::Mage as i32 => MAGE_STATS.as_ptr(),
+        x if x == Class::Priest as i32 => PRIEST_STATS.as_ptr(),
+        x if x == Class::Rogue as i32 => ROGUE_STATS.as_ptr(),
+        x if x == Class::Ranger as i32 => RANGER_STATS.as_ptr(),
+        x if x == Class::Paladin as i32 => PALADIN_STATS.as_ptr(),
+        x if x == Class::Druid as i32 => DRUID_STATS.as_ptr(),
+        x if x == Class::Bard as i32 => BARD_STATS.as_ptr(),
+        x if x == Class::Adventurer as i32 => ADVENTURER_STATS.as_ptr(),
+        x if x == Class::Monk as i32 => MONK_STATS.as_ptr(),
+        _ => panic!("Unknown class received"),
+    }
+}
+
+#[no_mangle]
+pub extern fn class_uses_magic(class: i32, magic_type: i32) -> u8 {
+    match magic_type {
+        x if x == MagicType::Arcane as i32 =>
+            (class == Class::Mage as i32 || class == Class::Adventurer as i32) as u8,
+        x if x == MagicType::Divine as i32 =>
+            (class == Class::Priest as i32 || class == Class::Paladin as i32) as u8,
+        x if x == MagicType::Nature as i32 =>
+            (class == Class::Druid as i32 || class == Class::Ranger as i32) as u8,
+        x if x == MagicType::Song as i32 =>
+            (class == Class::Bard as i32 || class == Class::Rogue as i32) as u8,
+        x if x == MagicType::Chakra as i32 =>
+            (class == Class::Monk as i32) as u8,
+        _ => panic!("Unknown class received"),
+    }
+}
+
 #[no_mangle]
 pub extern fn class_expfactor(class: i32) -> f32 {
     match class {
