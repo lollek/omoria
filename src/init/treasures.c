30b1a8b59e5d077d5183e196a6dd0fc770d67c84
Olle Aid Kvarnström <olle+git@iix.se>
2021-10-10 09:51:44 +0200

    Tweak sorting
    This is not a better solution - only simpler to the eye. It's maybe
    worth considering quicksort or similar function, but since I will move
    this to rust, it seems like a waste of time to create my own sorting
    function in C.

--- src/init/treasures.c
+++ src/init/treasures.c
@@ -12,27 +12,24 @@
 static bool sort_treasures(void) {
   ENTER(("sort_treasures", ""));
 
-  for (long gap = MAX_OBJECTS / 2; gap > 0; gap = gap / 2) {
-    for (long i = gap + 1; i <= MAX_OBJECTS; i++) {
-      for (long j = i - gap; j > 0; j = j - gap) {
-        long k = j + gap;
-        if (object_list[j].level > object_list[k].level) {
-          treasure_type tmp = object_list[j];
-          object_list[j] = object_list[k];
-          object_list[k] = tmp;
-        } else {
-          j = 0;
-        }
+  for (long i = 1; i < MAX_OBJECTS; ++i) {
+    for (long j = i + 1; j <= MAX_OBJECTS; ++j) {
+      if (object_list[i].level > object_list[j].level) {
+        treasure_type tmp = object_list[i];
+        object_list[i] = object_list[j];
+        object_list[j] = tmp;
       }
     }
   }
 
 #if DO_DEBUG
   /*  Verify that the sort worked */
-  long counter = 0;
-  for (long i = 1; 1 <= MAX_OBJECTS; ++i) {
+  int8_t counter = 0;
+  for (long i = 1; i <= MAX_OBJECTS; ++i) {
     if (counter > object_list[i].level) {
-      fprintf(stderr, "Error: sort_treasures failed\n");
+      fprintf(stderr, "Error: sort_treasures failed!\n"
+          "Index: %ld, left: %d, right: %d",
+          i, counter, object_list[i].level);
       return false;
     }
     counter = object_list[i].level;
