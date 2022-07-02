640182faacf09d32623d68890d92c3bbcd6062d9
Olle Aid Kvarnström <olle+git@iix.se>
2022-07-02 17:14:09 +0200

    Fix missing include in new creature.h

--- src/creature.h
+++ src/creature.h
@@ -1,6 +1,8 @@
 #ifndef CREATURE_H
 #define CREATURE_H
 
+#include "boolean.h"
+
 long find_mon(const char *virtual_name);
 void check_mon_lite(long y, long x);
 void multiply_monster(long y, long x, long z, boolean slp);
