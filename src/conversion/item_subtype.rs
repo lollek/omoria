a49b0e3de4fe12b76d39f5d914c0556f319b8701
Olle Aid Kvarnström <olle+git@iix.se>
2022-10-26 21:09:05 +0200

    Create generic function for subtype conversion

--- src/conversion/item_subtype.rs
+++ src/conversion/item_subtype.rs
@@ -1,1 +1,18 @@
+use crate::model::{item_subtype::ItemSubType, ItemType};
+
 pub mod light_source;
+
+pub fn to_usize(item_subtype: ItemSubType) -> usize {
+    match item_subtype {
+        ItemSubType::LightSource(subtype) => light_source::to_usize(subtype),
+    }
+}
+
+pub fn from_usize(item_type: ItemType, item_subtype: usize) -> Option<ItemSubType> {
+    match item_type {
+        ItemType::LightSource => {
+            light_source::from_usize(item_subtype).map(ItemSubType::LightSource)
+        }
+        _ => panic!("Unhandled item type {:?}", item_type),
+    }
+}
