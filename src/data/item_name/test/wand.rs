9b1fea86eaa2bd1ab4c27972f759f433be20e12f
Olle Aid Kvarnström <olle+git@iix.se>
2025-02-01 12:51:18 +0100

    fix: add wand names

---
+++ src/data/item_name/test/wand.rs
@@ -0,0 +1,20 @@
+use crate::data::item_name::generate;
+use crate::generate_item::template::WandTemplate;
+use crate::generate_item;
+
+#[test]
+fn test_wand_unidentified() {
+    let mut item = generate_item::generate(Box::new(WandTemplate::WandOfCloneMonster), 0);
+
+    item.set_identified(false);
+    assert_eq!(generate(&item), "Unknown wand");
+}
+
+#[test]
+fn test_wand_identified() {
+    let mut item = generate_item::generate(Box::new(WandTemplate::WandOfCloneMonster), 0);
+
+    item.set_identified(true);
+    item.p1 = 0;
+    assert_eq!(generate(&item), "Wand of Clone Monster (0 charges)");
+}
