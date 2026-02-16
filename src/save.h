04129f8f1cebc35ef9b341ce94f7b9880b2bbc2c
Olle Aid Kvarnström <olleai@axis.com>
2026-02-16 10:01:13 +0100

    switch some uint8 to bool

--- src/save.h
+++ src/save.h
@@ -1,19 +1,20 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
 
 /**
  * sav__save_character() - Saves the currenet character
  *
  * @returns: True on success, false on failure
  */
-uint8_t sav__save_character(void);
+bool sav__save_character(void);
 
 /**
  * sav__load_character() - Loads the given character
  *
  * @returns: True on success, false on failure
  */
-uint8_t sav__load_character(char const *player_name, int64_t player_uid);
+bool sav__load_character(char const *player_name, int64_t player_uid);
 
 void C_delete_character(void);
