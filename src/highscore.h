fe675cb1ef53f1356922d6896887bef2ea85b176
Olle Aid Kvarnström <olleai@axis.com>
2026-03-13 15:20:11 +0100

    add highscore at game start

--- src/highscore.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void C_highscore(uint8_t max_highscores);
--- src/highscore_extern.rs
+++
@@ -1,6 +0,0 @@
-use crate::highscore;
-
-#[no_mangle]
-pub extern "C" fn C_highscore(max: u8) {
-    highscore::highscore(max);
-}
